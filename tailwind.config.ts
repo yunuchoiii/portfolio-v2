65e2b49e4a73165d368700a474207965c0d7cb5f
Seowon Choi <107801496+yunuchoiii@users.noreply.github.com>
2025-11-24 16:55:31 +0900

    feat: 기본 세팅 및 스타일 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        green: {
+          10: "#C2FED8",
+          20: "#2CF599",
+          30: "#27DF8B",
+          40: "#20C077",
+          50: "#199F62",
+          60: "#117C4B",
+          70: "#095934",
+          80: "#03361E",
+          90: "#011509",
+        },
+        blue: {
+          10: "#E5F3FF",
+          20: "#BBE1FF",
+          30: "#65C7FF",
+          40: "#0AAEEA",
+          50: "#0797CB",
+          60: "#0478A3",
+          70: "#025777",
+          80: "#01344A",
+          90: "#00131D",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
